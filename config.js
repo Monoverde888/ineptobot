bcf37c736f3f18c80dd8c0c92160acbc34e63206
Monoverde <minecraftersov@gmail.com>
2020-06-04 21:28:47 -0400

    config.js quitado

--- config.js
+++
@@ -1,9 +0,0 @@
-// No quites las comillas al principio y fin de token y prefix
-let config = {
-    token: "NzExMzg4ODU5NDI2MTQ0MjU3.XtmckA.20giIv5agwwfsOT_ZdT9YgVIAhc", // Reemplaze esto con el token de tu de bot.
-    prefix: "!", // El prefix de su bot para indentificar sus comandos.
-  
-  }
-  
-  // Aqui exportamos el objeto config:
-  module.exports = config;
