01af7f5b5162489b9f8937f11459c9ab30e61741
Monoverde <minecraftersov@gmail.com>
2020-06-05 13:59:17 -0400

    make it better

--- config.js
+++
@@ -1,9 +0,0 @@
-// No quites las comillas al principio y fin de token y prefix
-let config = {
-    token: "NzExMzg4ODU5NDI2MTQ0MjU3.XtmeOg.Sem-APVNWau9fH3_Cpx0HXrqx-A", // Reemplaze esto con el token de tu de bot.
-    prefix: "!", // El prefix de su bot para indentificar sus comandos.
-  
-  }
-  
-  // Aqui exportamos el objeto config:
-  module.exports = config;
