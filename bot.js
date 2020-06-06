390f8dd83f30cc211446d2028b6e2897fd4ad5b3
Monoverde <minecraftersov@gmail.com>
2020-06-05 22:42:39 -0400

    añadido webserver de prueba

--- bot.js
+++ bot.js
@@ -1,3 +1,26 @@
+//ESTE CODIGO NO AFECTARA SU BOT, SCRIPT DE ARRANQUE 
+
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(`8080`);
+
+setInterval(() => {
+  http.get(`0.0.0.0`);
+}, 280000);
+
+//DESDE AQUI EMPIEZA A ESCRIBIR EL CODIGO PARA SU BOT
 const Discord = require('discord.js');
 const client = new Discord.Client();
 // El modulo fs se utiliza para leer los archivos y carpetas de un directorio:
