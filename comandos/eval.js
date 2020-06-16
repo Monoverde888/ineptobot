ee6753c834c8e5ebb06bb1397c39ded1cf1d259f
Monoverde <minecraftersov@gmail.com>
2020-06-15 22:16:48 -0400

    procfile

---
+++ comandos/eval.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+module.exports = (client, message, args) => {
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(config.prefix + "eval")) {
+    if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+})};
