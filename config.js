8df449d753ac8d9b93fe303f0ddaad1aa73d3536
Monoverde <minecraftersov@gmail.com>
2020-06-16 12:01:27 -0400

    no

---
+++ config.js
@@ -0,0 +1,10 @@
+// No quites las comillas al principio y fin de token y prefix
+let config = {
+    token: "NzExMzg4ODU5NDI2MTQ0MjU3.Xujrbw.4Qb8zb8atETYFB3YH_Rndy-6tLQ", // Reemplaza esto con el token de tu de bot.
+    prefix: "!", // El prefix de su bot para indentificar sus comandos.
+    ownerID: "322809311640289281",
+
+  }
+
+  // Aqui exportamos el objeto config:
+  module.exports = config;
